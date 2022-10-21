aedb393f34082843f6c738146d7186fefc4a9236
WSRWavedroids <Wavedroid13206@gmail.com>
2022-10-20 19:08:13 -0500

    Somehow managed to break everything and also not?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousPLUS.java
@@ -0,0 +1,219 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Robot;
+
+
+/**
+ * This is the autonomous mode. It moves the robot without us having to touch the controller.
+ * Previous programmers really sucked at explaining what any of this meant, so we're trying to do better.
+ *
+ */
+
+//todo Create function to convert tick values based on speed.
+public abstract class AutonomousPLUS extends LinearOpMode {
+
+    // This section tells the program all of the different pieces of hardware that are on our robot that we will use in the program.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public double speed = 0.5;
+
+    //DO NOT DELETE THIS LINE! CAPITALIZATION IS VERY IMPORTANT!!!
+    public Robot robot = new Robot();
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap, telemetry, this);
+        robot.encoderReset();
+        robot.encoderRunningMode();
+
+    }
+
+
+    public void moveRobotForward(int ticks) {
+        if (opModeIsActive()){
+            robot.setTargets("Forward", ticks);
+            robot.positionRunningMode();
+        }
+        robot.powerSet(speed);
+       /* if (moveRobotForward"") {
+            robot.powerSet(speed, speed, speed, speed)
+            );*/
+
+        while (opModeIsActive() &&
+                robot.isWheelsBusy()) {
+            robot.tellMotorOutput();
+            //nothings here
+        }
+
+        robot.stopAllMotors();
+        robot.encoderRunningMode();
+        robot.stopAllMotors();
+
+    }
+
+
+    public void moveRobotBackward(int ticks){
+        if (opModeIsActive()){
+            robot.setTargets("Backward", ticks);
+            robot.positionRunningMode();
+            robot.powerSet(speed);
+
+            while (opModeIsActive() &&
+                    robot.isWheelsBusy()) {
+                robot.tellMotorOutput();
+                //nothings here
+            }
+
+            robot.stopAllMotors();
+            robot.encoderRunningMode();
+
+
+        }
+
+    }
+    public void moveRobotLeft(int ticks) {
+
+        if (opModeIsActive()){
+            robot.setTargets("Left", ticks);
+            robot.positionRunningMode();
+            robot.powerSet(speed);
+
+            while (opModeIsActive() &&
+                    robot.isWheelsBusy()) {
+                robot.tellMotorOutput();
+                //nothings here
+            }
+
+            robot.stopAllMotors();
+            robot.encoderRunningMode();
+
+
+        }
+    }
+
+    public void moveRobotRight(int ticks) {
+
+        if (opModeIsActive()) {
+            robot.setTargets("Right", ticks);
+            robot.positionRunningMode();
+            robot.powerSet(speed);
+
+            while (opModeIsActive() &&
+                    robot.isWheelsBusy()) {
+                robot.tellMotorOutput();
+                //nothings here
+            }
+
+            robot.stopAllMotors();
+            robot.encoderRunningMode();
+
+
+        }
+    }
+
+    public void turnRobotRight(int ticks) {
+
+        if (opModeIsActive()) {
+            robot.setTargets("Right", ticks);
+            robot.positionRunningMode();
+            robot.powerSet(speed);
+
+            while (opModeIsActive() &&
+                    robot.isWheelsBusy()) {
+                robot.tellMotorOutput();
+                //nothings here
+            }
+
+            robot.stopAllMotors();
+            robot.encoderRunningMode();
+
+
+        }
+    }
+
+
+    public void turnDuckSpinnerBlue(double maxSeconds){
+     //   robot.duckSpinner.setPower(0.65);
+
+        while (opModeIsActive() && getRuntime() < maxSeconds) {
+            robot.tellMotorOutput();
+            //nothings here
+        }
+
+        robot.stopAllMotors();
+
+    }
+
+    public void turnDuckSpinnerRed(double maxSeconds){
+       // robot.duckSpinner.setPower(-0.65);
+
+        while (opModeIsActive() && getRuntime() < maxSeconds) {
+            robot.tellMotorOutput();
+            //nothings here
+        }
+
+        robot.stopAllMotors();
+
+    }
+
+    public void stopDuckSpinner(double maxSeconds){
+       // robot.duckSpinner.setPower(0);
+
+        while (opModeIsActive() && getRuntime() < maxSeconds) {
+            robot.tellMotorOutput();
+            //nothings here
+        }
+
+        robot.stopAllMotors();
+    }
+
+    public void prepareNextAction(long pause){
+        sleep(pause);
+        robot.encoderReset();
+    }
+
+    public void moveArm(String direction, double power){
+        if (direction == "Up"){
+       //     robot.clawArm.setDirection(DcMotor.Direction.REVERSE);
+        } else if (direction == "Down"){
+         //   robot.clawArm.setDirection(DcMotor.Direction.FORWARD);
+        }
+
+       // robot.clawArm.setPower(power);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,215 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import java.io.File;
+
+public class Robot {
+
+    public DcMotor frontLeftDrive;
+    public DcMotor frontRightDrive;
+    public DcMotor backLeftDrive;
+    public DcMotor backRightDrive;
+    //public DcMotor duckSpinner;
+    //public DcMotor clawArm;
+    //public CRServo claw;
+    public Telemetry telemetry;
+
+    //init and declare war
+    public OpMode opmode;
+    public HardwareMap hardwareMap;
+
+    //construct robot
+    public Robot() {
+
+    }
+
+    //Initialize motors and servos
+    public void init(HardwareMap hardwareMap, Telemetry telemetry, OpMode opmode){
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+        this.opmode = opmode;
+
+        // This section turns the names of the pieces of hardware into variables that we can program with.
+        // Make sure that the device name is the exact same thing you typed in on the configuration on the driver hub.
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+        //duckSpinner = hardwareMap.get(DcMotor.class, "duckSpinner");
+        //clawArm = hardwareMap.get(DcMotor.class, "clawArm");
+        //claw = hardwareMap.get(CRServo.class, "claw");
+
+
+        this.frontLeftDrive = frontLeftDrive;
+        this.frontRightDrive = frontRightDrive;
+        this.backLeftDrive = backLeftDrive;
+        this.backRightDrive = backRightDrive;
+        //this.duckSpinner = duckSpinner;
+        //this.clawArm = clawArm;
+        //this.claw = claw;
+
+        // This section sets the direction of all of the motors. Depending on the motor, this may change later in the program.
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        //duckSpinner.setDirection(DcMotor.Direction.FORWARD);
+        //clawArm.setDirection(DcMotor.Direction.FORWARD);
+
+        // This tells the motors to chill when we're not powering them.
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+
+    public boolean isWheelsBusy(){
+        if(backLeftDrive.isBusy() || frontLeftDrive.isBusy() || frontRightDrive.isBusy() || backRightDrive.isBusy()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public void turnDuckSpinner(double power){
+       // duckSpinner.setPower(power);
+        telemetry.addData("Ducks", "Whee!");
+    }
+    public void stopDuckSpinner(){
+      //  duckSpinner.setPower(0);
+        telemetry.addData("Ducks", "Whee!");
+    }
+
+    public void stopAllMotors() {
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+
+    public void setTargets(String direction, int ticks) {
+
+        if (direction == "Right"){
+            frontLeftDrive.setTargetPosition(-ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Left"){
+            frontLeftDrive.setTargetPosition(ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Forward"){
+            frontLeftDrive.setTargetPosition(-ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Backward") {
+            frontLeftDrive.setTargetPosition(ticks - frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks - frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks - backRightDrive.getCurrentPosition());
+
+           /* else if (direction == "turnRight" )   {
+                frontLeftDrive.setTargetPosition(ticks + frontLeftDrive.getCurrentPosition());
+                frontRightDrive.setTargetPosition(ticks - frontRightDrive.getCurrentPosition());
+                backLeftDrive.setTargetPosition(ticks + backLeftDrive.getCurrentPosition());
+                backRightDrive.setTargetPosition(ticks - backRightDrive.getCurrentPosition());
+            }*/
+        }
+    }
+
+    public void positionRunningMode(){
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    /* lic void powerSet(double frontLeftSpeed, double frontRightSpeed, double backLeftSpeed, double backRightSpeed) {
+            frontLeftDrive.setPower(frontLeftSpeed);
+            frontRightDrive.setPower(frontRightSpeed);
+            backLeftDrive.setPower(backLeftSpeed);
+            backRightDrive.setPower(backRightspeed);*/
+    public void powerSet(double speed) {
+        frontLeftDrive.setPower(speed);
+        frontRightDrive.setPower(speed);
+        backLeftDrive.setPower(speed);
+        backRightDrive.setPower(speed);
+
+    }
+
+    public void openAndCloseClaw (double position){
+       // telemetry.addData("ServoPort", "Port: " + claw.getPortNumber());
+      //  claw.getController().setServoPosition(claw.getPortNumber(), position);
+
+        if (position == 0){
+            telemetry.addData("Claw", "Closed");
+        } else if (position >= 0.3){
+            telemetry.addData("Claw", "Open");
+        }
+    }
+
+    public void encoderRunningMode(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void encoderReset(){
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void tellMotorOutput(){
+        telemetry.addData("Motors", String.format("FL Power(%.2f) FL Location (%d) FL Target (%d)", frontLeftDrive.getPower(), frontLeftDrive.getCurrentPosition(), frontLeftDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("FR Power(%.2f) FR Location (%d) FR Target (%d)", frontRightDrive.getPower(), frontRightDrive.getCurrentPosition(), frontRightDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("BL Power(%.2f) BL Location (%d) BL Target (%d)", backLeftDrive.getPower(), backLeftDrive.getCurrentPosition(), backLeftDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("BR Power(%.2f) BR Location (%d) BR Target (%d)", backRightDrive.getPower(), backRightDrive.getCurrentPosition(), backRightDrive.getTargetPosition()));
+     //   telemetry.addData("Motors", "Duck Spinner (%.2f)", duckSpinner.getPower());
+       // telemetry.addData("Motors", "Arm (%.2f)", clawArm.getPower());
+        telemetry.update();
+    }
+
+    public double inchesToTicks(double inches){
+        // returns the inches * ticks per rotation / wheel circ
+        return ((inches/12.25) * 537.6 / .5);
+        //todo Reference that 1 inch ~= 50 ticks
+    }
+
+    public void moveArm(String direction, double power){
+        if (direction == "Up"){
+          //  clawArm.setDirection(DcMotor.Direction.REVERSE);
+        } else if (direction == "Down"){
+           // clawArm.setDirection(DcMotor.Direction.FORWARD);
+        }
+    //    clawArm.setPower(power);
+    }
+
+    public void holdArm(){
+       // clawArm.setDirection(DcMotor.Direction.REVERSE);
+       // clawArm.setPower(0.1);
+    }
+
+
+}
